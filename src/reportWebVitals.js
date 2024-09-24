ea1208b13bd2a2f35fc236ee79b854e2e8af67dc
Ivan <ibixrivera16@gmail.com>
2024-09-23 19:49:36 -0700

    webvital update

---
+++ src/reportWebVitals.js
@@ -0,0 +1,12 @@
+import { getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
